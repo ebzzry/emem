be3bf0a3fad1cdc69e49eacd439fc84e317d2465
Rommel Martínez <rommel.martinez@valmiz.com>
2025-02-06 02:17:14 +0800

    [version] bump to 0.2.51

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject emem "0.2.50-SNAPSHOT"
+(defproject emem "0.2.51-SNAPSHOT"
   :description "A trivial Markdown to HTML converter"
   :url "https://github.com/ebzzry/emem"
   :license {:name "Eclipse Public License"
